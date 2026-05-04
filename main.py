a5266fc6ce34ccd69f32a6a95eb516a4c5c562a4
Andrey <andreykolosov05@gmail.com>
2026-05-04 03:56:34 +0300

    Add PATCH and DELETE endpoints

--- main.py
+++ main.py
@@ -24,6 +24,15 @@ class TaskCreateSchema(BaseModel):
     title: str
 
 
+class BookCreateSchema(BaseModel):
+    book: str
+
+
+class TaskUpdateSchema(BaseModel):
+    title: str | None = None
+    completed: bool | None = None
+
+
 tasks: list[TaskSchema] = []
 
 
@@ -45,8 +54,23 @@ def create_task(payload: TaskCreateSchema) -> TaskSchema:
     return new_task
 
 
-class BookCreate(BaseModel):
-    book: str
+@app.patch("/tasks/{task_id}")
+def update_task(task_id: str, payload: TaskUpdateSchema):
+    for task in tasks:
+        if task.id == task_id:
+            if payload.title is not None:
+                task.title = payload.title
+            if payload.completed is not None:
+                task.completed = payload.completed
+            return task
+
+
+@app.delete("/tasks/{task_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_task(task_id: str):
+    for task in tasks:
+        if task.id == task_id:
+            tasks.remove(task)
+            return
 
 
 book = None
@@ -58,7 +82,7 @@ def read_book():
 
 
 @app.post("/book")
-def create_book(payload: BookCreate) -> str:
+def create_book(payload: BookCreateSchema) -> str:
     global book
     book = payload.book
     return book
