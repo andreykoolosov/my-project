958e0683b8fd3ee698fadbbf93c8e19c31c725da
Andrey <andreykolosov05@gmail.com>
2026-05-21 17:39:06 +0300

    Update data

--- app/main.py
+++ app/main.py
@@ -1,22 +1,11 @@
-from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.models.base import Base
-from app.db.session import engine
 from app.api.routers.task import router as task_router
 from app.api.routers.category import router as category_router
 from app.core.config import settings
 
 
-@asynccontextmanager
-async def lifespan(_: FastAPI):
-    print("START")
-    Base.metadata.create_all(bind=engine)
-    yield
-    print("END")
-
-
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 app.include_router(router=task_router)
 app.include_router(router=category_router)
 
